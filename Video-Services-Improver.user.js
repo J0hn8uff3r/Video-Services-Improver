0ab75a068bae62aa36511a0355f41a31503a01b9
J0hn8uff3r <bufferoverflow@hotmail.es>
2020-02-20 00:08:22 +0100

    Improvements added to southpark.cc.com video host
    Prevent the annoying default scrolldown when spacebar is pressed
    Added UP/DOWN volume control and LEFT/RIGHT currentTime backward/forward control

--- Video-Services-Improver.user.js
+++ Video-Services-Improver.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Video Services Improver
 // @namespace    https://github.com/J0hn8uff3r
-// @version      1.3
+// @version      1.4
 // @description  Skips ads, intros and wait time on Amazon Prime Video and improves other video sites
 // @author       J0hn8uff3r
 // @match        https://www.primevideo.com/*
@@ -11,6 +11,57 @@
 // @updateURL    https://gist.github.com/J0hn8uff3r/499350cc4db92478d0e32a22e0e56a33/raw/a6fb1907ee2144851771d097729ebc78ff7a7cce/Amazon-Skipper.user.js
 // @grant        none
 // ==/UserScript==
+
+//Prevent the annoying default scrolldown
+$('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+});
+
+document.onkeydown = key;
+function key(e) {
+
+    e = e || window.event;
+    video = document.querySelector('video');
+
+    if (e.keyCode == '32') {
+        console.log("SPACEBAR")
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+    if (e.keyCode == '38') {
+        console.log("UP ARROW")
+        if (video.volume + 0.05 > 1.0) {
+            video.volume = 1.0;
+        } else {
+            video.muted = false;
+            video.volume += 0.05;
+        }
+    }
+    else if (e.keyCode == '40') {
+        console.log("DOWN ARROW")
+        if (video.volume != 0) {
+            if (video.volume - 0.05 < 0) {
+                video.volume = 0;
+            } else {
+                video.volume -= 0.05;
+            }
+        }
+    }
+    else if (e.keyCode == '37') {
+        console.log("LEFT ARROW")
+        video.currentTime -= 5;
+    }
+    else if (e.keyCode == '39') {
+        console.log("RIGHT ARROW")
+        video.currentTime += 5;
+    }
+
+}
+
 
 $(document).ready(function () {
     $(document).dblclick(function () {
@@ -61,29 +112,29 @@ document.addEventListener("wheel", function (e) {
     var direction = Math.sign(e.deltaY);
 
     if (window.location.href.indexOf("southpark.cc.com/full-episodes/") != -1) {
-        volume_element = document.querySelector('video');
+        video = document.querySelector('video');
         console.log("SOUTH PARK")
     }
 
     if (window.location.href.indexOf("primevideo.com/detail/") != -1) {
-        volume_element = document.querySelector('video[width="100%"]');
+        video = document.querySelector('video[width="100%"]');
         console.log("PRIMEVIDEO")
     }
 
     if (direction == "-1") {
         console.log("UP")
-        if (volume_element.volume + 0.05 > 1.0) {
-            volume_element.volume = 1.0;
+        if (video.volume + 0.05 > 1.0) {
+            video.volume = 1.0;
         } else {
-            volume_element.muted = false;
-            volume_element.volume += 0.05;
+            video.muted = false;
+            video.volume += 0.05;
         }
-    } else if (volume_element.volume != 0) {
+    } else if (video.volume != 0) {
         console.log("DOWN")
-        if (volume_element - 0.05 < 0) {
-            volume_element.volume = 0;
+        if (video.volume - 0.05 < 0) {
+            video.volume = 0;
         } else {
-            volume_element.volume -= 0.05;
+            video.volume -= 0.05;
         }
     }
 });
